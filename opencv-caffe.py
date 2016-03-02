17197cbcdb067647aab97cdcb798ae06b332bf4e
Gautam Shine <gshine@stanford.edu>
2016-03-01 18:57:35 -0800

    Confusion matrix and mean subtraction
    Creates a confusion matrix given a mapping and subtracts mean from input
    images.

--- opencv-caffe.py
+++ opencv-caffe.py
@@ -11,7 +11,7 @@
 # International Conference on Multimodal Interaction (ICMI), Seattle, Nov. 2015
 ###############################################################################
 
-import os, sys, time, re, glob
+import os, shutil, sys, time, re, glob
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
@@ -21,17 +21,6 @@ import caffe
 ###############################################################################
 # Caffe VGG_S net emotion classification
 ###############################################################################
-
-caffe_root = '/home/gshine/Documents/caffe/'
-sys.path.insert(0, caffe_root + 'python')
-
-plt.rcParams['figure.figsize'] = (10, 10)
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-net_root = '.'
-
-categories = [ 'Angry' , 'Disgust' , 'Fear' , 'Happy'  , 'Neutral' ,  'Sad' , 'Surprise']
 
 def showimage(img):
     if img.ndim == 3:
@@ -54,27 +43,110 @@ def vis_square(data, padsize=1, padval=0):
     
     showimage(data)
 
-cur_net_dir = 'VGG_S_rgb'
+def mod_dim(img, x=256, y=256, c=3):
+    resized = caffe.io.resize_image(img, (x,y,c)) # (256, 256, 3)
+    rearranged = np.swapaxes(np.swapaxes(resized, 1, 2), 0, 1) # (3, 256, 256)
+    return rearranged
 
-mean_filename=os.path.join(net_root,cur_net_dir,'mean.binaryproto')
-proto_data = open(mean_filename, "rb").read()
-a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
-mean  = caffe.io.blobproto_to_array(a)[0]
+def compute_mean(input_list):
+    # If no data supplied, use mean supplied with pretrained model
+    if len(input_list) == 0:
+        net_root = '.'
+        net_dir = 'VGG_S_rgb'
+        mean_filename=os.path.join(net_root, net_dir, 'mean.binaryproto')
+        proto_data = open(mean_filename, "rb").read()
+        a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
+        mean  = caffe.io.blobproto_to_array(a)[0]
+    else:
+        x,y,c = 256,256,3
+        mean = np.zeros((c, x, y))
+        for img_file in input_list:
+            img = caffe.io.load_image(img_file)
+            img = mod_dim(img, x, y, c)
+            mean += img
+        mean /= len(input_list)
+        plt.imshow(np.swapaxes(np.swapaxes(mean, 0, 1), 1, 2))
+        plt.show()
+    return mean
 
-net_pretrained = os.path.join(net_root,cur_net_dir,'EmotiW_VGG_S.caffemodel')
-net_model_file = os.path.join(net_root,cur_net_dir,'deploy.prototxt')
-VGG_S_Net = caffe.Classifier(net_model_file, net_pretrained,
+def make_net(mean):
+    caffe_root = '/home/gshine/Documents/caffe/'
+    sys.path.insert(0, caffe_root + 'python')
+
+    plt.rcParams['figure.figsize'] = (10, 10)
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'
+
+    categories = [ 'Angry' , 'Disgust' , 'Fear' , 'Happy'  , 'Neutral' ,  'Sad' , 'Surprise']
+
+    net_root = '.'
+    net_dir = 'VGG_S_rgb'
+
+    net_pretrained = os.path.join(net_root, net_dir, 'EmotiW_VGG_S.caffemodel')
+    net_model_file = os.path.join(net_root, net_dir, 'deploy.prototxt')
+    VGG_S_Net = caffe.Classifier(net_model_file, net_pretrained,
                        mean=mean,
                        channel_swap=(2,1,0),
                        raw_scale=255,
                        image_dims=(256, 256))
+    return VGG_S_Net, categories
 
-def classify_emotions(input_list, show_faces, show_neurons):
-    for image_file in input_list:
-        input_image = caffe.io.load_image(image_file)
+def plot_confusion_matrix(cm, names, title='Confusion Matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    
+    # Add labels to confusion matrix:
+    tick_marks = np.arange(len(names))
+    plt.xticks(tick_marks, names, rotation=45)
+    plt.yticks(tick_marks, names)
+    
+    plt.tight_layout()
+    plt.ylabel('Correct label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+def confusion(results, categories):
+    map_categories = {
+        'HA': categories.index('Happy'),
+        'SA': categories.index('Sad'),
+        'NE': categories.index('Neutral'),
+        'AN': categories.index('Angry'),
+        'FE': categories.index('Fear'),
+        'DI': categories.index('Disgust'),
+        'SU': categories.index('Surprise')
+    }
+    matrix = np.zeros((7,7))
+    for label, pred in results:
+        matrix[map_categories[label],pred] += 1
+    
+    # Display percent accuracy
+    accuracy = float(np.trace(matrix))/len(results)
+    print('Accuracy: ', accuracy*100, '%')
+    print('Confusion Matrix: ')
+    print(matrix)
+    # Plot the confusion matrix
+    plot_confusion_matrix(matrix, categories)
+
+def classify_emotions(input_list, show_confusion, show_faces, show_neurons):
+    # Compute mean
+    mean = compute_mean(input_list)
+    
+    # Create VGG_S net with mean
+    VGG_S_Net, categories = make_net(mean)
+
+    # Classify images in directory
+    conf_mat = [] # confusion matrix
+    for img_file in input_list:
+        input_image = caffe.io.load_image(img_file)
         # TODO: test larger crops + oversampling
         prediction = VGG_S_Net.predict([input_image], oversample=False)
-        print 'prediction: {0}'.format(categories[prediction.argmax()])
+
+        if show_confusion:
+            label = img_file.split('.')[1][0:2]
+            conf_mat.append((label, prediction.argmax()))
+
+        print(img_file.split('/')[-1] + ': ' + categories[prediction.argmax()])
         
         # TODO: Prediction currently breaks after 1st face if plotting is on
         if show_faces:
@@ -94,6 +166,9 @@ def classify_emotions(input_list, show_faces, show_neurons):
             plt.show(block=False)
             time.sleep(1)
             plt.close('all')
+
+    if show_confusion:
+        confusion(conf_mat, categories)
 
 ###############################################################################
 # OpenCV face recognition and segmentation
@@ -137,48 +212,66 @@ def rgb(bgr_img):
     rgb_img = cv.merge([r,g,b])     # switch it to rgb
     return rgb_img
 
-def faceCrop(loc, faceCascade):
+def faceCrop(loc, faceCascade, color=True):
     # Find all images in location
-    imgList = glob.glob(loc)
+    imgList = glob.glob(loc+'/*')
     if len(imgList) <= 0:
         print 'No images found.'
         return
-    
+
     # Iterate through images
     face_list = []
     for img in imgList:
+        if os.path.isdir(img):
+            continue
         pil_img = Image.open(img)
-        cv_img  = cv.cvtColor(np.array(pil_img), cv.COLOR_RGB2BGR)
+        if color:
+            cv_img  = cv.cvtColor(np.array(pil_img), cv.COLOR_RGB2BGR)
+        else:
+            cv_img = np.array(pil_img)
         scaled_img, faces = DetectFace(cv_img, faceCascade, gray=False)
         
         # Iterate through faces
         n=1
         for face in faces:
             cropped_cv_img = imgCrop(scaled_img, face)
-            cropped_cv_img = rgb(cropped_cv_img)
+            if color:
+                cropped_cv_img = rgb(cropped_cv_img)
             fname, ext = os.path.splitext(img)
             cropped_pil_img = Image.fromarray(cropped_cv_img)
-            save_name = fname + '_crop' + str(n) + ext
+            save_name = loc + '/cropped/' + fname.split('/')[-1] + '_crop' + str(n) + ext
             cropped_pil_img.save(save_name)
             face_list.append(save_name)
             n += 1
 
     return face_list
 
+###############################################################################
+# Main
+###############################################################################
+
 def purge(dir, pattern):
     for f in os.listdir(dir):
     	if re.search(pattern, f):
     	    os.remove(os.path.join(dir, f))
 
+def mkdir(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.mkdir(dir)
+
+dir = 'datasets/jaffe'
+
 # Clean up cropped images from directory
-purge('images/', 'crop')
+#purge('datasets/jaffe', 'crop')
+mkdir(dir+'/cropped')
 
 # Load Haar cascade files containing features
 cascPath = 'haarcascade_frontalface_default.xml'
 faceCascade = cv.CascadeClassifier(cascPath)
 
 # Perform detection and cropping
-input_list = faceCrop('images/*', faceCascade)
+input_list = faceCrop(dir, faceCascade, color=False)
 
 # Perform classification
-classify_emotions(input_list, show_faces=False, show_neurons=False)
+classify_emotions(input_list, show_confusion=True, show_faces=False, show_neurons=False)
